6d1144c09fa51528d3b1041b77f78011348fe1e6
JV-AZevedo <joaobatata.azevedo.13@gmail.com>
2025-11-24 19:49:12 -0400

    .

--- livros_avaliacao.py
+++ livros_avaliacao.py
@@ -1,6 +1,13 @@
+
+
 with open("livros.txt","r") as livros:
-    with open("livros_avaliacao.txt","w"):
+    with open("livros_avaliacao.txt","w") as notas:
         for linhas in livros:
             dados = linhas.strip().split(",")
-            n = float(input("Nota para o livro " +str(dados[1])+ " de "+str(dados[0])+ " Escrito por"+str(dados[2])+": "))
+        try:
+            n = float(input("Nota para o livro" +str(dados[1])+ " de "+str(dados[0])+ " Escrito por"+str(dados[2])+": "))
+            s = input("Status de leitura do livro" +str(dados[1])+ " de "+str(dados[0])+ " Escrito por"+str(dados[2])+" (Lido/Lendo/Na fila): ")
+            notas.write(f"{n}, {s}\n")
+        except:
+           
               
